4670e40e6669e1fdcb85b3bd04d959884290f2ec
zeetee1235 <zeetee1235@gmail.com>
2025-05-06 11:44:10 +0900

    .

---
+++ class2/python/스택_수열.py
@@ -0,0 +1,36 @@
+import sys
+
+x = []
+
+for line in sys.stdin:
+    try:
+        x.append(line.strip())
+    except:
+        break
+
+n = int(x.pop(0))
+target = list(map(int, x))
+
+stack = []
+result = []
+cur = 1
+possible = True
+
+for i in range(n):
+    while cur <= target[i]:
+        stack.append(cur)
+        result.append("+")
+        cur += 1
+
+    if stack[-1] == target[i]:
+        stack.pop()
+        result.append("-")
+    else:
+        possible = False
+        break
+    
+if  possible:
+    print("\n".join(result))
+else:
+    print("NO")
+
