6b4de76a11a14950dfcb7a95c167b62b16e81355
zeetee1235 <zeetee1235@gmail.com>
2025-08-14 02:28:38 +0900

    .

---
+++ class3/숨바꼭질.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+s = sys.stdin.readline()
+lst = list(map(int, s.split()))
+n = lst[0]
+k = lst[1]
+
+dq = deque([n])
+MAX = 100000
+dist = [-1] * (MAX + 1)
+dist[n] = 0
+
+if n == k:
+    print(0)
+
+while dq:
+    x = dq.popleft()
+    for i in (x - 1, x + 1, x * 2):
+        if 0 <= i <= MAX and dist[i] == -1:
+            dist[i] = dist[x] + 1
+            if i  == k:
+                print(dist[i])
+                exit()
+            dq.append(i)
