a7656cc13939dc14dd6507139fc14e1b6e33e774
zeetee1235 <zeetee1235@gmail.com>
2025-08-18 05:42:23 +0900

    .

--- Z.py
+++ Z.py
@@ -5,3 +5,16 @@ x = [list(map(int, i.split())) for i in x]
 n = x[0][0]
 r = x[0][1]
 c = x[0][2]
+count = 0
+
+def count_z(n, r, c):
+    w = n ** 2
+    if r <= (w / 2):
+        pass
+    else:
+        pass
+    if c <= (w / 2):
+        pass
+    else:
+        pass
+    
