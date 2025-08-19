dbcd34a936361e5545a2cd06cd073276f7f11718
zeetee1235 <zeetee1235@gmail.com>
2025-08-20 07:05:20 +0900

    .

---
+++ class3/회의실_배정.py
@@ -0,0 +1,18 @@
+import sys
+
+x = sys.stdin.read().splitlines()
+x = [list(map(int, i.split())) for i in x]
+n = int(x[0][0])
+x.pop(0)
+
+x.sort(key=lambda x: (x[1]))
+
+count = 0
+last_end_time = 0
+    
+for start, end in x:
+    if start >= last_end_time:
+        count += 1
+        last_end_time = end
+    
+print(count)
