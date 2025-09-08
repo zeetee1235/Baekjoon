26b3a9fb8861c2f2bf12beb03bdd85d322afa986
zeetee1235 <zeetee1235@gmail.com>
2025-09-09 02:09:11 +0900

    .

---
+++ ex/피보나치_수_3.py
@@ -0,0 +1,23 @@
+n = int(input())
+mod = 1000000
+# 피사노 주기 10^k 일때 (k > 2) 15 * 10 ^(k - 1)
+period = 1500000
+
+def fibonacci_list(m):
+    # return list of Fibonacci numbers modulo mod up to index m
+    if m == 0:
+        return [0]
+    dp = [0] * (m + 1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2, m + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % mod
+    return dp
+
+arr = fibonacci_list(period)
+print(arr[n % period])
+
+
+    
+    
+
